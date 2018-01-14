87001f23dc38369dcd6e561e3e143dc8401e7c94
Pedro Alves <alves.pedro0@gmail.com>
2018-01-14 17:40:29 +0100

    dotnet

---
+++ dotnet/HelloWorldApp/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\nWhat is your name? ");
+            var name = string.Empty;
+            do
+            {
+                name = Console.ReadLine();
+            } while (string.IsNullOrEmpty(name));
+                        
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
